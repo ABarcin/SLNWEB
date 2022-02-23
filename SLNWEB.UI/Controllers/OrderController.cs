f14307755bccb80938eff16d3eaae18ed7d202b5
Elif DURMAZ <elif.durmaz@poi.com.tr>
2022-02-24 01:46:45 +0300

    sipariş sayfası get

--- SLNWEB.UI/Controllers/OrderController.cs
+++ SLNWEB.UI/Controllers/OrderController.cs
@@ -16,26 +16,63 @@ namespace SLNWEB.UI.Controllers
             return View();
         }
 
-        IOrderDAL _orderDAL;
+        //IOrderDAL _orderDAL;
         CategoryDAL categoryDal = new CategoryDAL();
         ProductDAL productDal = new ProductDAL();
-        //ICategoryDAL _catagoryDal;
+        EmployeeDAL employeeDal = new EmployeeDAL();
+        ShipperDAL shipperDal = new ShipperDAL();
 
         [HttpGet]
         public ActionResult SatisYap()
         {
             
             ViewBag.Catagories = new SelectList(categoryDal.GetAllCategories());
-            SatisVM satisVM = new SatisVM() { 
-      
-            
+            SatisVM satisVM = new SatisVM() {
+                Products = ProductGetir(productDal.GetAllProductVMByCategoryID(1)),
+                Employees=EmployeeGetir(employeeDal.GettAllEmployee()),
+                Shippers=ShipperGetir(shipperDal.GetAllShipper()),
+                Order=new OrderVM()
             };
+
             
             return View(satisVM);
         }
 
+        private List<SelectListItem> ShipperGetir(List<ShipperVM> shipperVMs)
+        {
+            return (from s in shipperVMs
+                    select new SelectListItem()
+                    {
+                        Text = s.CompanyName.ToUpper(),
+                        Value = s.ShipperID.ToString()
+                    }
+            ).ToList();
+        }
+
+        private List<SelectListItem> EmployeeGetir(List<EmployeeVM> employeeVMs)
+        {
+            return (from e in employeeVMs
+                    select new SelectListItem()
+                    {
+                        Text = (e.FirstName+" "+e.LastName).ToUpper(),
+                        Value = e.EmployeeID.ToString()
+                    }
+                    ).ToList();
+        }
+
+        private List<SelectListItem> ProductGetir(List<ProductVM> productVMs)
+        {
+            return (from p in productVMs
+                    select new SelectListItem()
+                    {
+                        Text=p.ProductName.ToUpper(),
+                        Value=p.ProductID.ToString()
+                    }
+                    ).ToList();
+        }
+
         [HttpPost]
-        public ActionResult SatisYap(SatisYapVM satis)
+        public ActionResult SatisYap(SatisVM satis)
         {
             //order tabla ekle
             return View();
