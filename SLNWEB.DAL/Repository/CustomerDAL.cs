d4f7cb2d078e2d70474a2b91dc845911977512bc
buraktoglu <buraktoglu@gmail.com>
2022-02-24 00:25:34 +0300

    bug fixes

---
+++ SLNWEB.DAL/Repository/CustomerDAL.cs
@@ -0,0 +1,24 @@
+﻿using SLNWEB.Core;
+using SLNWEB.DAL.Mapping;
+using SLNWEB.DAO.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLNWEB.DAL.Repository
+{
+    public class CustomerDAL : EntityRepository<Customer, NorthwindEntities>, ICustomerDAL
+    {
+        public CustomerVM GetCustomer()
+        {
+            return new CustomerMapping().CustomerToCustomerVM(GetAll(x => x.CustomerID == "ALFKI").SingleOrDefault());
+        }
+
+        public List<CustomerVM> GetCustomerList()
+        {
+            return new CustomerMapping().ListCustomerToListCustomerVM(GetAll().ToList());
+        }
+    }
+}
