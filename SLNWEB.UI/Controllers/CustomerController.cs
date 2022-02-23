b9628970ea663cc3bb56c80ae04a8fd36cda2ff5
buraktoglu <buraktoglu@gmail.com>
2022-02-23 21:38:53 +0300

    configurations

---
+++ SLNWEB.UI/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using SLNWEB.DAO.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SLNWEB.UI.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View(new CustomerVM());
+        }
+    }
+}
